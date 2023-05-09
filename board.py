90ea0796797d13e8e3e724c755439df1eed72420
Lyle Wong <lylereturns@gmail.com>
2023-05-09 18:40:31 +0100

    Added cell functionality: - Cell's can either be "X" or "O". - Only unchosen cells can be picked by the player. - Added methods to draw "O" or "X" based on the nature of the cell.

--- board.py
+++ board.py
@@ -6,14 +6,19 @@ from pygame.mouse import get_pos as pygame_mouse_get_pos
 from pygame.mouse import get_pressed as pygame_mouse_get_pressed
 from pygame import Rect as pygame_Rect
 
+from random import choice as random_choice
+
 class Board:
 
     def __init__(self, board_dimensions, ):
+
         self.surface = pygame_display_get_surface()
         self.dimensions = board_dimensions
-        self.cell_dimensions = (self.dimensions[0] // 3, self.dimensions[1] // 3)
 
+        self.cell_dimensions = (self.dimensions[0] // 3, self.dimensions[1] // 3)
         self.cells = self.create_cells()
+
+        self.current_turn = random_choice(("O", "X")) # Player can be "O" or "X" 
         
         # print([(cell.rect.x, cell.rect.y) for cell in self.cells])
 
@@ -32,13 +37,29 @@ class Board:
         mouse_pos = pygame_mouse_get_pos()
         mouse_rect = pygame_Rect(mouse_pos[0], mouse_pos[1], 1, 1)
 
+        # Left mouse click
         if pygame_mouse_get_pressed()[0]:
-            cell_collided = mouse_rect.collidelist(self.cells)
+            
+            if self.released_button == True:
+        
+                self.released_button = False
+                cell_collided = mouse_rect.collidelist(self.cells)
 
-            if cell_collided != -1:
-                pass
-            print(cell_collided)
+                # An unchosen cell
+                if cell_collided != -1 and self.cells[cell_collided].nature == None:
 
+                    # Set this cell to either "X" or "O"
+                    self.cells[cell_collided].nature = self.current_turn
+
+                    # ADD METHOD TO CHECK WINNER HERE
+
+                    # Switch turn
+                    self.current_turn = "X" if self.current_turn == "O" else "O"
+
+        # Released left mouse click
+        else:
+            self.released_button = True
+    
     def draw_grid(self):
         
         # Vertical
@@ -60,7 +81,13 @@ class Board:
                             width = 5
                             )
     
+    def draw_cells(self):
+
+        for cell in self.cells:
+            cell.draw()
+        
     def run(self):
 
         self.draw_grid()
+        self.draw_cells()
         self.handle_cell_collisions()
